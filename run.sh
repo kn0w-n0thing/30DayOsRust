65f44a06c0814a450a8516425086fca515e4f8c2
clldaxigua <clldaxigua@126.com>
2022-12-16 00:21:58 +0800

    Make boot image from nasm codes.

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# assemble helloos.nas to a raw image
+nasm -f bin helloos.nas -o helloos.img
+
+# boot an x86 32bit virtual machine with a raw image
 qemu-system-i386 -drive format=raw,if=floppy,index=0,readonly=true,file=helloos.img -boot order=a
